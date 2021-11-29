adf98d71f690ae754a4d5ec7cb8cc281ef012dab
KauztoKuro <ikkikurogane70@gmail.com>
2021-11-29 15:03:02 +0630

    Brand section

---
+++ src/components/brand/import.js
@@ -0,0 +1,9 @@
+import google from '../../components/assets/google.png';
+import slack  from '../../components/assets/slack.png';
+import atlassian from '../../components/assets/atlassian.png';
+import dropbox from '../../components/assets/dropbox.png';
+import shopify from '../../components/assets/shopify.png';
+
+export {
+    google, slack, atlassian, dropbox, shopify
+}
