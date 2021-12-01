932a130e97b263428fe3f0683896034952e25555
KauztoKuro <ikkikurogane70@gmail.com>
2021-12-01 10:22:53 +0630

    Blog Section

---
+++ src/containers/blog/imports.js
@@ -0,0 +1,9 @@
+import blog01 from '../../components/assets/blog01.png';
+import blog02  from '../../components/assets/blog02.png';
+import blog03 from '../../components/assets/blog03.png';
+import blog04 from '../../components/assets/blog04.png';
+import blog05 from '../../components/assets/blog05.png';
+
+export {
+    blog01, blog02, blog03, blog04, blog05
+}
